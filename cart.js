4dfccb95ab07a179dd9d5f60dd3c2541fbf6dca1
unknown <phaneendraguduru6@gmail.com>
2025-05-15 09:12:00 +0530

    Added navigated link in the cart page;

--- cart.js
+++ cart.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function () {
   emptyMessage.innerHTML = `
         <div  class="empty" >
             <h2 class="empty">Your Cart is Empty</h2>
-            <button onclick="" class="continueshopping">
+            <button onclick="window.location.href='home.html'" class="continueshopping">
                 <i class="fa fa-arrow-left"></i> Continue Shopping
             </button>
         </div>
